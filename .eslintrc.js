9bef156915179600f585254ee299cd514ce2e0ab
Simon Lydell <simon.lydell@gmail.com>
2017-05-21 21:22:59 +0200

    Update npm dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     "id-blacklist": "off",
     "no-console": "off",
     "no-use-before-define": ["error", { functions: false }],
+    "no-tabs": "error",
     strict: "off",
     "prettier/prettier": "error"
   },
